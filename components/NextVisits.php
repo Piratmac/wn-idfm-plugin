c18e738bd826a619e0a47ee2938f49d62cdb0b4a
Piratmac <piratmac@gmail.com>
2018-05-30 22:16:41 +0200

    Corrected ignored destination filtering

--- components/NextVisits.php
+++ components/NextVisits.php
@@ -31,14 +31,22 @@ class NextVisits extends ComponentBase
                   $query->where('departure_time', '>=', Carbon\Carbon::now()->addMinutes(-1 * IDFMSettings::get('displayPastXMinutes')))
                         ->orWhere('error_message', '!=', '')->where('record_time', '>=', Carbon\Carbon::now()->addHours(-1 * IDFMSettings::get('displayErrorsForXHours')));
                 })
-                # This part removes the destinations marked as "ignored" by the users
+ /*This actually doesn't work because the ignored destinations will be applied on all of the monitored stops (rather than the one where it was defined)
+                  # This part removes the destinations marked as "ignored" by the users
                 ->whereNotIn('destination_id', function ($query) {
                   $query->select('ignored_destination_id')
                         ->from('piratmac_idfm_monitored_stop_ignored_destination')
                         ->join('piratmac_idfm_monitored_stop', 'piratmac_idfm_monitored_stop.monitored_stop_id', '=', 'piratmac_idfm_monitored_stop_ignored_destination.monitored_stop_id');
-                  });
-        }],
-        'visits.stop', 'visits.line', 'visits.destination')
+                  })*/
+                ;
+
+        }])
         ->get();
+      # This filters the visits according to the ignored destinations
+      $this->monitored_stops->each(function($monitoredStop, $key) {
+        $this->monitored_stops[$key]->visits = $monitoredStop->visits->reject(function ($visit) use ($monitoredStop) {
+          return in_array($visit->destination->idfm_id, $monitoredStop->ignored_destinations->pluck('idfm_id')->toArray());
+        });
+      });
     }
 }
